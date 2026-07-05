b0e77d1d2c64e02dd5916bd148087a4e036630e9
Kamil1developer <igologramma@mail.ru>
2026-07-05 16:26:06 +0300

    добавил интерфейс и класс реализации для UserRepository

--- src/main/java/com/example/weatherviewer/repository/UserRepository.java
+++ src/main/java/com/example/weatherviewer/repository/UserRepository.java
@@ -1,33 +1,15 @@
 package com.example.weatherviewer.repository;
 
 import com.example.weatherviewer.entity.User;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+public interface UserRepository {
 
+    public Optional<User> findByLogin(String userName);
 
-public class UserRepository {
-    @PersistenceContext
-    private  EntityManager entityManager;
-
-    public Optional<User> findByLogin(String userName){
-        return entityManager.createQuery("select u from User u where u.login = :userName", User.class)
-                .setParameter("userName", userName)
-                .getResultStream()
-                .findFirst();
-    }
-
-    public List<User> findAll(){
-        return entityManager.createQuery("select u from User u", User.class)
-                .getResultList();
-    }
-    public void save(User user){
-        entityManager.persist(user);
-    }
+    public List<User> findAll();
+    public void save(User user);
 }
---
+++ src/main/java/com/example/weatherviewer/repository/impl/JpaUserRepository.java
@@ -0,0 +1,33 @@
+package com.example.weatherviewer.repository.impl;
+
+import com.example.weatherviewer.entity.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+
+
+public class JpaUserRepository {
+    @PersistenceContext
+    private  EntityManager entityManager;
+
+    public Optional<User> findByLogin(String userName){
+        return entityManager.createQuery("select u from User u where u.login = :userName", User.class)
+                .setParameter("userName", userName)
+                .getResultStream()
+                .findFirst();
+    }
+
+    public List<User> findAll(){
+        return entityManager.createQuery("select u from User u", User.class)
+                .getResultList();
+    }
+    public void save(User user){
+        entityManager.persist(user);
+    }
+}
